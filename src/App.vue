57bfdfd9ec4712ffcaa763d5af95b1b3f616d961
gregorioAgrela <103526753+gregorioAgrela@users.noreply.github.com>
2022-09-27 16:52:57 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <Counter :start="10"  } /> -->
+  <Indecision />
+</template>
+<script>
+// import Counter from './components/CounterNuevo.vue'
+import Indecision from './components/Indecision.vue'
+export default {
+  name: 'App',
+  components: {
+    Indecision
+    // Counter
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/CounterNuevo.vue
@@ -0,0 +1,66 @@
+<template>
+  <h2> {{ customeTitle }}</h2>
+  <p> {{ counter }} <sub>2</sub> = {{ getSquareValue() }} </p>
+  <div>
+    <button @click="increase"> +1 </button>
+    <button v-on:click="decrease"> -1 </button>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    title: String,
+    start: {
+      type: Number,
+      required: true,
+      default: 100,
+      validator (value) {
+        return value >= 0
+      }
+    }
+  },
+  // name: 'Vue/Multi-Word-Component'
+  data () {
+    return {
+      name: 'ounter',
+      counter: parseInt(this.start)
+    }
+  },
+  methods: {
+    getSquareValue () {
+      return this.counter * this.counter
+    },
+    increase () {
+      this.counter += 1
+    },
+    decrease () {
+      this.counter -= 1
+    }
+  },
+  computed: {
+    squareCouinter () {
+      return this.counter * this.counter
+    },
+    customeTitle () {
+      return this.title || 'valor por defecto'
+    }
+  }
+
+}
+</script>
+<style>
+  button {
+    background-color: #5aa67b;
+    border-radius:5px;
+    border: 1px solid white;
+    color:white;
+    cursor: pointer;
+    margin: 0 5px;
+    padding: 5px 15px;
+    transition: 0.3s ease-in-out
+  }
+  button:hover {
+    background-color: #5aa67b;
+    transition: 0.3s ease-in-out
+  }
+</style>
